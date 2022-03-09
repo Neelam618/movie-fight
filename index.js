ebd855e806909d28dbcaed63b2d897465c2a6ac6
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-09 15:25:44 +0530

    display serached movies in dom

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const fetchData = async (searchTerm) => {
             s: searchTerm            //for searching the movie containing searchTerm
         }
     })
-    console.log(response.data);
+    return response.data.Search            //returns a promise
 }
 
 const input = document.querySelector('input')
 
-const onInput = event => {
-    fetchData(event.target.value)          //send input value to api to search movie
+const onInput = async event => {                    //add async sice we are adding await
+    const movies = await fetchData(event.target.value)          //send input value to api to search movie        //returns promise so add await
+    for (let movie of movies) {
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <img src="${movie.Poster}">
+            <h1>${movie.Title}</h1>
+        `;
+        document.querySelector('#target').appendChild(div)
+    }
 }
 input.addEventListener('input', debounce(onInput, 1000))     //for searching movie for every single keypress
