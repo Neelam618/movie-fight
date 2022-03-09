8491dac7d4fb8d29c928efbd149a4951852f2d3e
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-09 16:11:33 +0530

    widget creation for autocomplete

--- index.js
+++ index.js
@@ -10,18 +10,32 @@ const fetchData = async (searchTerm) => {
     }
     return response.data.Search            //returns a promise
 }
-
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = async event => {                    //add async sice we are adding await
     const movies = await fetchData(event.target.value)          //send input value to api to search movie        //returns promise so add await
+    dropdown.classList.add('is-active')
+
     for (let movie of movies) {
-        const div = document.createElement('div')
-        div.innerHTML = `
+        const option = document.createElement('a')
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
             <img src="${movie.Poster}">
             <h1>${movie.Title}</h1>
         `;
-        document.querySelector('#target').appendChild(div)
+        resultsWrapper.appendChild(option)
     }
 }
 input.addEventListener('input', debounce(onInput, 1000))     //for searching movie for every single keypress
