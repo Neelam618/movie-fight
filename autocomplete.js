1a26ffc8728c53d67b642b4d2fe9cb374360f0a8
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-10 12:00:04 +0530

    refactoring and adding reusable autocomplete

---
+++ autocomplete.js
@@ -0,0 +1,42 @@
+const createAutoComplete = ({ root, renderOption }) => {               //application specific data is sent through config object  //root is key
+    root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`
+    const input = root.querySelector('input')
+    const dropdown = root.querySelector('.dropdown')
+    const resultsWrapper = root.querySelector('.results')
+
+    const onInput = async event => {                    //add async sice we are adding await
+        const movies = await fetchData(event.target.value)          //send input value to api to search movie        //returns promise so add await
+        if (!movies.length) {
+            dropdown.classList.remove('is-active')
+            return
+        }
+        dropdown.classList.add('is-active')
+        resultsWrapper.innerHTML = ''            //to clear the last search results and avoid overriding results
+        for (let movie of movies) {
+            const option = document.createElement('a')
+            option.classList.add('dropdown-item')
+            option.innerHTML = renderOption(movie)
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active')    //close dropdown on selecting option
+                input.value = movie.Title               //to update movie title in input field when selected
+                onMovieSelect(movie)
+            })
+            resultsWrapper.appendChild(option)
+        }
+    }
+    input.addEventListener('input', debounce(onInput, 1000))     //for searching movie for every single keypress
+
+    document.addEventListener('click', event => {         //when user clicks outside dropdown
+        if (!root.contains(event.target)) {              //autocomplete element is not clicked
+            dropdown.classList.remove('is-active')
+        }
+    })
+}
--- index.js
+++ index.js
@@ -10,49 +10,15 @@ const fetchData = async (searchTerm) => {
     }
     return response.data.Search            //returns a promise
 }
-const root = document.querySelector('.autocomplete')
-root.innerHTML = `
-    <label><b>Search for a movie</b></label>
-    <input class="input">
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`
-const input = document.querySelector('input')
-const dropdown = document.querySelector('.dropdown')
-const resultsWrapper = document.querySelector('.results')
 
-const onInput = async event => {                    //add async sice we are adding await
-    const movies = await fetchData(event.target.value)          //send input value to api to search movie        //returns promise so add await
-    if (!movies.length) {
-        dropdown.classList.remove('is-active')
-        return
-    }
-    dropdown.classList.add('is-active')
-    resultsWrapper.innerHTML = ''            //to clear the last search results and avoid overriding results
-    for (let movie of movies) {
+createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+    renderOption(movie) {                  //how movie item in search results will look
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster   //for broken images show nothing
-        const option = document.createElement('a')
-        option.classList.add('dropdown-item')
-        option.innerHTML = `
+        return `
             <img src="${imgSrc}">
-            <h1>${movie.Title}</h1>
+            ${movie.Title} (${movie.Year})
         `;
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active')    //close dropdown on selecting option
-            input.value = movie.Title               //to update movie title in input field when selected
-            onMovieSelect(movie)
-        })
-        resultsWrapper.appendChild(option)
-    }
-}
-input.addEventListener('input', debounce(onInput, 1000))     //for searching movie for every single keypress
-
-document.addEventListener('click', event => {         //when user clicks outside dropdown
-    if (!root.contains(event.target)) {              //autocomplete element is not clicked
-        dropdown.classList.remove('is-active')
     }
 })
 
